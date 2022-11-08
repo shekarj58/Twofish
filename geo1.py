785fa4bd0ad2b8eea86c7b03f91a772f87700be2
shekarj58 <rajashekar.reddy0108@gmail.com>
2022-11-08 19:35:28 +0530

    Create geo1.py

---
+++ geo1.py
@@ -0,0 +1,15 @@
+"""import geocoder
+from twofish import Twofish
+i='me'
+g=geocoder.ipinfo(i)
+print(g.latlng)
+
+
+"""
+n=int(input())
+arr=str(n)
+if arr=arr[::-1]:
+    print("palindrome")
+
+    
+    
