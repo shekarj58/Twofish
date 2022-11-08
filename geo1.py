17aa5bd5b0865595a75384459877ccd568cf7adb
shekarj58 <117739805+shekarj58@users.noreply.github.com>
2022-11-08 19:40:12 +0530

    Update geo1.py

--- geo1.py
+++ geo1.py
@@ -1,15 +1,9 @@
-"""import geocoder
+import geocoder
 from twofish import Twofish
 i='me'
 g=geocoder.ipinfo(i)
 print(g.latlng)
 
-
-"""
-n=int(input())
-arr=str(n)
-if arr=arr[::-1]:
-    print("palindrome")
 
     
     
